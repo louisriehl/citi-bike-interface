b1fd14e042d59ea343705fde4738ec0bc66a9ebd
Louis Riehl <louisriehl@gmail.com>
2018-12-06 14:33:15 -0500

    can now hover over component for extra information

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react'
 
 class Bike extends Component {
+    constructor () {
+        super()
+        this.state = {
+            isHovered: false
+        }
+        this.mouseEntered = this.mouseEntered.bind(this)
+        this.mouseLeft = this.mouseLeft.bind(this)
+    }
     render() {
+        const elementIsHovered = this.state.isHovered
+        let extraInfo
+
+        if (elementIsHovered) {
+            extraInfo = <div>
+                <p>Available Docks: {this.props.availableDocks}</p>
+                <p>Lat/Long: {this.props.lat}, {this.props.long}</p>
+                <p>Last Communication: {this.props.lastComm}</p>
+            </div>
+        }
         return(
-            <div className='station'>
+            <div className='station' onMouseEnter={this.mouseEntered} onMouseLeave={this.mouseLeft}>
                 <p>Station Name: {this.props.name}</p>
                 <p>Percent Bikes Available: {((this.props.bikes / this.props.totalDocks) * 100).toFixed(2)}%</p>
+                {extraInfo}
             </div>
         )
+    }
+
+    mouseEntered () {
+        this.setState({isHovered: true})
+    }
+
+    mouseLeft () {
+        this.setState({isHovered: false})
     }
 }
 
